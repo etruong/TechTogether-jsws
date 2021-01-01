458623c424267fec2495c0a28bd53cf0eee1e579
Elisa <elisat15@uw.edu>
2020-12-31 22:24:35 -0800

    set up workshop repo

---
+++ intro-demo/index.js
@@ -0,0 +1,42 @@
+// STORING DATA //
+
+/* VARIABLES: 
+Create variables that stores a string, number, and boolean */
+
+/* OPERATIONS: 
+Using the variables you created previously, concatenate them and log a sentence */
+
+/* OBJECTS: 
+Create an object that stores information about a person (ie name, age, location) */
+
+/* ARRAYS: 
+Create an array that stores 5 different types of your favorite food */
+
+/* TOPICS NOT COVERED: Classes */
+
+// CONTROL OF FLOW //
+
+/* FOR LOOP: 
+Loop through the previous array you just made and log each food item */
+
+/* IF/ELSE: 
+Create an if/else chain depending on the value of a random number
+    if the number is less than 10 log out "Bing", 
+    else if the number is even log out "Ping",
+    otherwise log out "Wazz Up" */
+
+/* TOPICS NOT COVERED: While Loops */
+
+// FUNCTIONS //
+
+/* Create a function that logs out "Hello World" and call it 3 times */
+
+/* Create a function that has a name parameter that logs out "Hello name" 
+    where name is the value that is passed in by the user. 
+    Call it 3 times with different names. */
+
+/* Create an anonymous function that has 2 number parameters that 
+    returns the 2 numbers added together  */
+
+
+/* TOPICS NOT COVERED: Arrow Functions, filter, forEach, map */
