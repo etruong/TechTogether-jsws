cab1b2ef8dc8e6f1795bff9e1fa0e7eab24ac4bb
Elisa <elisat15@uw.edu>
2021-01-22 20:18:17 -0800

    add detail instructions

---
+++ website-demo/index.js
@@ -0,0 +1,8 @@
+// Measure Container Functionality:
+// (grabbing/manipulating the DOM, event handling)
+
+// Dynamically generate the serious symptoms content:
+// (selecting/adding/manipulating the DOM)
+
+// Show Results container when clicking button:
+// (event handling, selecting/manipulating the DOM)
